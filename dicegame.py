8f3eca9ce4b6e001fd89d87b2f16f115628f8d58
jonlwil <70457126+jonlwil@users.noreply.github.com>
2020-12-15 14:04:40 -0800

    Add files via upload

--- dicegame.py
+++ dicegame.py
@@ -1,15 +1,17 @@
 from random import choices
 ntrials=15000
 player1wins=0
-roll_dice=[1,2,3,4,5,6]
+p2dice=2
+p1dice=3
+total=0
 for i in range(ntrials):
-    player2=[1,2,3,4,5,6]
-    player2=choices(roll_dice, weights=None, k=2)
-    if player2[0]==player[2]:
-        player1=[1,2,3,4,5,6]
-        player1=choices(roll_dice,weights=None, k=3)
-        player1.sort(reverse=True)
-    if player1[0]+player1[1]>player2[0]+player2[1]:
+    dice=choices(range(1,7), k=p2dice)
+    dice.sort(reverse=True)
+    total=total+dice[0]+dice[1]
+    if dice[0]==dice[1]:
+        continue
+    dice2=choices(range(1,7), k=p1dice)
+    dice.sort(reverse=True)
+    if dice[0]+dice[1]< dice2[1]+dice2[2]:
         player1wins=player1wins+1
-ratio=player1wins/ntrials
-print(ratio)
+print("Fairness ratio=",player1wins/ntrials)
